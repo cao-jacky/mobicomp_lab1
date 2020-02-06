3fe96d298567d935de24ce496bc222bd5d234f75
Jacky Cao <probablyameow+github@gmail.com>
2020-02-06 22:47:07 +0200

    updated with a way to add a time based reminder
    - need to add a way to set alarms for each reminder

--- app/src/main/java/com/example/mobicomp_lab1/MainActivity.kt
+++ app/src/main/java/com/example/mobicomp_lab1/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mobicomp_lab1
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -37,6 +39,10 @@ class MainActivity : AppCompatActivity() {
     private var db: AppDatabase? = null
     private var ReminderDao: ReminderDao? = null
 
+    private val REQUEST_CODE = 100
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val timeClass = TimeActivity()
         val mapClass = MapActivity()
@@ -49,26 +55,35 @@ class MainActivity : AppCompatActivity() {
 
         Observable.fromCallable {
 
-            val fab: View = findViewById(R.id.addItemButton)
-            fab.setOnClickListener { view ->
-                Snackbar.make(view, "Add an item", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .show()
+            val aTR: View = findViewById(R.id.addTimeReminder)
+            aTR.setOnClickListener { view ->
+                val new_reminder_intent = Intent(this, TimeActivity::class.java)
+                startActivity(new_reminder_intent)
+
             }
 
-//            db = AppDatabase.getAppDataBase(context = this)
-//            ReminderDao = db?.reminderDao()
-
-//            var item1 = Reminder(location = "here", time = System.currentTimeMillis(), message = "test")
-//
-//            with(ReminderDao){
-//                this?.insertAll(item1)
-//            }
             remindersListView = findViewById<ListView>(R.id.remindersListView)
             var queryDatabase = db?.reminderDao()?.getAll()
-//            Log.d("debugging", queryDatabase.toString())
-
             val listItems = arrayOf(queryDatabase?.size)
+            val adapter = listAdapter(this, ArrayList(queryDatabase!!))
+            remindersListView.adapter = adapter
+
+            // Creating the pending intent to send to the BroadcastReceiver
+//            alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//            val intent = Intent(this, AlarmReceiver::class.java)
+//            pendingIntent = PendingIntent.getBroadcast(this, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+//
+//            // Setting the specific time for the alarm manager to trigger the intent, in this example, the alarm is set to go off at 23:30, update the time according to your need
+//            val calendar = Calendar.getInstance()
+//            calendar.timeInMillis = System.currentTimeMillis()
+//            calendar.set(Calendar.HOUR_OF_DAY, 20)
+//            calendar.set(Calendar.MINUTE, 52)
+//
+//            alarmManager.set(
+//                AlarmManager.RTC,
+//                calendar.timeInMillis,
+//                pendingIntent
+//            )
 
 //            for (i in 0 until queryDatabase?.size!!) {
 //                val reminderItem = queryDatabase?.get(i)
@@ -76,146 +91,153 @@ class MainActivity : AppCompatActivity() {
 //                val rITime = reminderItem.time.toString()
 //                val rILoc = reminderItem.location.toString()
 //                val rIMess = reminderItem.message.toString()
-
+//
 //                val rIArray = arrayOf(rIUID, rITime, rILoc, rIMess)
-
+//
 //                listItems[i] = rIUID?.toInt()
 //            }
 
-//            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
-//            remindersListView.adapter = adapter
-
-            val adapter = listAdapter(this, ArrayList(queryDatabase!!))
-            remindersListView.adapter = adapter
+//            alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//            for (i in 0 until queryDatabase?.size!!) {
+//                var alarmItemIntent = Intent(this, AlarmReceiver::class.java)
+//                pendingIntent = PendingIntent.getBroadcast(this, i, alarmItemIntent, 0)
+//
+//                val reminderItem = queryDatabase?.get(i)
+//                val rITime = reminderItem.time
+//
+//                val cal = Calendar.getInstance()
+//                cal.timeInMillis = rITime!!
+//
+////                // Setting the specific time for the alarm manager to trigger the intent, in this example, the alarm is set to go off at 23:30, update the time according to your need
+////                val calendar = Calendar.getInstance()
+////                calendar.timeInMillis = System.currentTimeMillis()
+////                calendar.set(Calendar.HOUR_OF_DAY, 20)
+////                calendar.set(Calendar.MINUTE, 52)
+//
+//                alarmManager.set(
+//                    AlarmManager.RTC,
+//                    cal.timeInMillis,
+//                    pendingIntent
+//                )
+//            }
 
         }.subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe()
 
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-//        val listItems = arrayOfNulls<String>(recipeList.size)
+//        val timeButton = findViewById<Button>(R.id.time_button)
+//        timeButton?.setOnClickListener()
+//        {
+//            val sysCurrTime = timeClass.pullCurrentTime()
+//            Toast.makeText(this@MainActivity,
+//                sysCurrTime, Toast.LENGTH_LONG).show() }
 //
-//        for (i in 0 until recipeList.size) {
-//            val recipe = recipeList[i]
-//            listItems[i] = recipe.title
+//        val locButton = findViewById<Button>(R.id.location_button)
+//        locButton?.setOnClickListener()
+//        {
+//            getLastLocation()
 //        }
 //
-//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
-//        listView.adapter = adapter
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-        val timeButton = findViewById<Button>(R.id.time_button)
-        timeButton?.setOnClickListener()
-        {
-            val sysCurrTime = timeClass.pullCurrentTime()
-            Toast.makeText(this@MainActivity,
-                sysCurrTime, Toast.LENGTH_LONG).show() }
-
-        val locButton = findViewById<Button>(R.id.location_button)
-        locButton?.setOnClickListener()
-        {
-            getLastLocation()
-        }
-
-        val playMediaButton = findViewById<Button>(R.id.sound_button)
-        playMediaButton?.setOnClickListener()
-        {
-            try {
-                val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-                val r = RingtoneManager.getRingtone(applicationContext, notification)
-                r.play()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-
-        }
+//        val playMediaButton = findViewById<Button>(R.id.sound_button)
+//        playMediaButton?.setOnClickListener()
+//        {
+//            try {
+//                val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//                val r = RingtoneManager.getRingtone(applicationContext, notification)
+//                r.play()
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//
+//        }
 
     }
 
-    @SuppressLint("MissingPermission")
-    fun getLastLocation() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
+//    @SuppressLint("MissingPermission")
+//    fun getLastLocation() {
+//        if (checkPermissions()) {
+//            if (isLocationEnabled()) {
+//
+//                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+//                    var location: Location? = task.result
+//                    if (location == null) {
+//                        requestNewLocationData()
+//                    } else {
+//                        findViewById<TextView>(R.id.latTextView).text = location.latitude.toString()
+//                        findViewById<TextView>(R.id.lonTextView).text = location.longitude.toString()
+//                    }
+//                }
+//            } else {
+//                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+//                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//                startActivity(intent)
+//            }
+//        } else {
+//            requestPermissions()
+//        }
+//    }
 
-                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
-                    var location: Location? = task.result
-                    if (location == null) {
-                        requestNewLocationData()
-                    } else {
-                        findViewById<TextView>(R.id.latTextView).text = location.latitude.toString()
-                        findViewById<TextView>(R.id.lonTextView).text = location.longitude.toString()
-                    }
-                }
-            } else {
-                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
-                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(intent)
-            }
-        } else {
-            requestPermissions()
-        }
-    }
+//    @SuppressLint("MissingPermission")
+//    private fun requestNewLocationData() {
+//        var mLocationRequest = LocationRequest()
+//        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+//        mLocationRequest.interval = 0
+//        mLocationRequest.fastestInterval = 0
+//        mLocationRequest.numUpdates = 1
+//
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+//        mFusedLocationClient!!.requestLocationUpdates(
+//            mLocationRequest, mLocationCallback,
+//            Looper.myLooper()
+//        )
+//    }
+//
+//    private val mLocationCallback = object : LocationCallback() {
+//        override fun onLocationResult(locationResult: LocationResult) {
+//            var mLastLocation: Location = locationResult.lastLocation
+//            findViewById<TextView>(R.id.latTextView).text = mLastLocation.latitude.toString()
+//            findViewById<TextView>(R.id.lonTextView).text = mLastLocation.longitude.toString()
+//        }
+//    }
 
-    @SuppressLint("MissingPermission")
-    private fun requestNewLocationData() {
-        var mLocationRequest = LocationRequest()
-        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        mLocationRequest.interval = 0
-        mLocationRequest.fastestInterval = 0
-        mLocationRequest.numUpdates = 1
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        mFusedLocationClient!!.requestLocationUpdates(
-            mLocationRequest, mLocationCallback,
-            Looper.myLooper()
-        )
-    }
-
-    private val mLocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            var mLastLocation: Location = locationResult.lastLocation
-            findViewById<TextView>(R.id.latTextView).text = mLastLocation.latitude.toString()
-            findViewById<TextView>(R.id.lonTextView).text = mLastLocation.longitude.toString()
-        }
-    }
-
-    private fun isLocationEnabled(): Boolean {
-        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    private fun checkPermissions(): Boolean {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
-            PERMISSION_ID
-        )
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == PERMISSION_ID) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                getLastLocation()
-            }
-        }
-    }
+//    private fun isLocationEnabled(): Boolean {
+//        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+//            LocationManager.NETWORK_PROVIDER
+//        )
+//    }
+//
+//    private fun checkPermissions(): Boolean {
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED &&
+//            ActivityCompat.checkSelfPermission(
+//                this,
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    private fun requestPermissions() {
+//        ActivityCompat.requestPermissions(
+//            this,
+//            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+//            PERMISSION_ID
+//        )
+//    }
+//
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+//        if (requestCode == PERMISSION_ID) {
+//            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+//                getLastLocation()
+//            }
+//        }
+//    }
 }
 
--- app/src/main/java/com/example/mobicomp_lab1/TimeActivity.kt
+++ app/src/main/java/com/example/mobicomp_lab1/TimeActivity.kt
@@ -1,9 +1,70 @@
 package com.example.mobicomp_lab1
 
-import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.CalendarView
+import android.widget.TextView
+import android.widget.TimePicker
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobicomp_lab1.dao.ReminderDao
+import com.example.mobicomp_lab1.dataBase.AppDatabase
+import com.example.mobicomp_lab1.entity.Reminder
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.*
 
-class TimeActivity : Activity() {
-    fun pullCurrentTime(): String {
-        return System.currentTimeMillis().toString()
+
+class TimeActivity : AppCompatActivity() {
+
+    private var db: AppDatabase? = null
+    private var ReminderDao: ReminderDao? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_time_reminder_view)
+
+        val aTR: View = findViewById(R.id.submitTimeReminder)
+        aTR.setOnClickListener { view ->
+            val timeReminderText: TextView = findViewById(R.id.timeReminderText)
+            val tRTString: String = timeReminderText.text.toString()
+
+            val timeReminderDate: CalendarView = findViewById(R.id.dateView)
+            val tRDString: Long = timeReminderDate.date
+
+            val timeReminderTime: TimePicker = findViewById(R.id.timePicker)
+            val hour: Int = timeReminderTime.hour
+            val min: Int = timeReminderTime.minute
+
+            val cal = Calendar.getInstance()
+            cal.timeInMillis = tRDString
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, min)
+
+            val setDateTime: Long = cal.timeInMillis
+
+            db = AppDatabase.getAppDataBase(context = this)
+            ReminderDao = db?.reminderDao()
+
+            Observable.fromCallable {
+                var newTimeReminder = Reminder(null, location = "0", time = setDateTime, message = tRTString)
+
+                with(ReminderDao) {
+                    this?.insertAll(newTimeReminder)
+                }
+            }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
+
+            val newMainActivity = Intent(this, MainActivity::class.java)
+            finish()
+            startActivity(newMainActivity)
+
+//            var queryDatabase = db?.reminderDao()?.getAll()
+//            val adapter = listAdapter(this, ArrayList(queryDatabase!!))
+//            adapter.notifyDataSetChanged()
+
+        }
     }
 }
